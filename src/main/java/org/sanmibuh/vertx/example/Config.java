0ac204c7aa1329bc7ec700e5a4ad3b03b4e75809
David Belmez Vicente <sanmibuh@gmail.com>
2019-01-24 16:50:53 +0100

    [APP-CONFIG]: Application configuration - Configuring your Vert.x application

---
+++ src/main/java/org/sanmibuh/vertx/example/Config.java
@@ -0,0 +1,11 @@
+package org.sanmibuh.vertx.example;
+
+public final class Config {
+
+  public static final String HTTP_PORT_KEY = "http.port";
+  public static final int DEFAULT_HTTP_PORT = 8080;
+
+  private Config() {
+    //Constant class
+  }
+}
--- src/main/java/org/sanmibuh/vertx/example/MyFirstVerticle.java
+++ src/main/java/org/sanmibuh/vertx/example/MyFirstVerticle.java
@@ -7,15 +7,20 @@ public class MyFirstVerticle extends AbstractVerticle {
 
   @Override
   public void start(final Future<Void> fut) {
+    final int port = getPort();
     vertx
         .createHttpServer()
         .requestHandler(r -> r.response().end("<h1>Hello from my first Vert.x 3 application</h1>"))
-        .listen(8080, result -> {
+        .listen(port, result -> {
           if (result.succeeded()) {
             fut.complete();
           } else {
             fut.fail(result.cause());
           }
         });
+  }
+
+  private int getPort() {
+    return config().getInteger(Config.HTTP_PORT_KEY, Config.DEFAULT_HTTP_PORT);
   }
 }
