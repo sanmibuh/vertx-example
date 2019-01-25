c3082c79ec5084f8cf6d4c254dbd637f4f325cad
David Belmez Vicente <sanmibuh@gmail.com>
2019-01-25 13:12:40 +0100

    [REST]: Use VertX Web

--- src/main/java/org/sanmibuh/vertx/example/MyFirstVerticle.java
+++ src/main/java/org/sanmibuh/vertx/example/MyFirstVerticle.java
@@ -2,22 +2,35 @@ package org.sanmibuh.vertx.example;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MyFirstVerticle extends AbstractVerticle {
 
   @Override
   public void start(final Future<Void> fut) {
-    final int port = getPort();
+    final Router router = Router.router(vertx);
+
+    router.route("/").handler(routingContext -> {
+      final HttpServerResponse response = routingContext.response();
+      response
+          .putHeader("content-type", "text/html")
+          .end("<h1>Hello from my first Vert.x 3 application</h1>");
+    });
+
     vertx
         .createHttpServer()
-        .requestHandler(r -> r.response().end("<h1>Hello from my first Vert.x 3 application</h1>"))
-        .listen(port, result -> {
-          if (result.succeeded()) {
-            fut.complete();
-          } else {
-            fut.fail(result.cause());
-          }
-        });
+        .requestHandler(router)
+        .listen(
+            getPort(),
+            result -> {
+              if (result.succeeded()) {
+                fut.complete();
+              } else {
+                fut.fail(result.cause());
+              }
+            }
+        );
   }
 
   private int getPort() {
